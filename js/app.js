70060fd7554da9e12010ecb0408a3a0ea5094f89
ikolokotronis <ioanniskolokotronis1@gmail.com>
2022-02-20 12:49:59 +0100

    Find city functionality

--- js/app.js
+++ js/app.js
@@ -53,5 +53,79 @@ document.addEventListener('DOMContentLoaded', event => {
         event.target.parentElement.parentElement.remove()
     })
 
+    find_city_form.addEventListener('submit', event => {
+        body_query.classList.add('loading')
+        event.preventDefault()
+        const searchInputValue = document.querySelector('#search').value
+        const appContainer = document.querySelector('#app')
+        get_current_weather(searchInputValue).then(data => {
+            console.log(data)
+            body_query.classList.remove('loading')
+            const city_name = data.location.name
+            const current_temp = data.current.temp_c
+            const current_pressure = `${data.current.pressure_mb} hPa`
+            const current_humidity = `${data.current.humidity}%`
+            const current_wind_speed = `${data.current.wind_kph} km/h`
+
+            const new_div = document.createElement('div')
+            new_div.classList.add('module__weather')
+            new_div.innerHTML =
+                `
+                
+            <div class="module module__weather">
+                <button class="btn btn--icon btn--close"><i class="material-icons">close</i></button>
+        
+                <div class="weather">
+                    <div class="weather__icon"><img src=""/></div>
+        
+                    <div class="weather__info">
+                        <div class="city">
+                            <span class="city__name">${city_name}</span> <span class="btn btn--icon"><i class="material-icons">edit</i></span>
+                        </div>
+                        <div class="temperature"><span id="current_temperature_value" class="temperature__value">${current_temp}</span>&deg;C</div>
+                    </div>
+        
+                    <ul class="weather__details">
+                        <li><img src="../assets/icons/pressure.svg"/> <span class="pressure__value">${current_pressure}</span></li>
+                        <li><img src="../assets/icons/humidity.svg"/> <span class="humidity__value">${current_humidity}</span></li>
+                        <li><img src="../assets/icons/wind-speed.svg"/> <span class="wind-speed__value">${current_wind_speed}</span></li>
+                    </ul>
+        
+                    <ul class="weather__forecast">
+                        <li>
+                            <span class="day">Tuesday</span> <img src="../assets/icons/clear-day.svg"/>
+                            <span class="temperature"><span class="temperature__value">10.0</span>&deg;C</span>
+                        </li>
+        
+                        <li>
+                            <span class="day">Wednesday</span> <img src="../assets/icons/snow.svg"/>
+                            <span class="temperature"><span class="temperature__value">-2.3</span>&deg;C</span>
+                        </li>
+        
+                        <li>
+                            <span class="day">Thursday</span> <img src="../assets/icons/rain.svg"/>
+                            <span class="temperature"><span class="temperature__value">4.8</span>&deg;C</span>
+                            </li>
+        
+                        <li>
+                            <span class="day">Friday</span> <img src="../assets/icons/cloudy.svg"/>
+                            <span class="temperature"><span class="temperature__value">5.0</span>&deg;C</span>
+                        </li>
+        
+                        <li>
+                            <span class="day">Saturday</span> <img src="../assets/icons/hail.svg"/>
+                            <span class="temperature"><span class="temperature__value">8.6</span>&deg;C</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+       
+                `
+
+            appContainer.append(new_div)
+
+        })
+
+    })
 
 })
